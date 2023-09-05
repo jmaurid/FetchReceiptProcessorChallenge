e58edbf3bdbf6b275069be27c50882f1ede483e2
jmaurid <jmaurid@hotmail.com>
2023-09-04 18:56:28 -0600

    Initial Commit

---
+++ src/main/java/com/fetch/exercise/fetchexercise/FetchExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.fetch.exercise.fetchexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fetch/exercise/fetchexercise/FetchExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.exercise.fetchexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
